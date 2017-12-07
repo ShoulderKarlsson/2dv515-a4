fa3bdbda686d7c5a94671c0d5e110be9ed7154a1
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-12-07 17:49:55 +0100

    feat: bayes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import bayers.BayersMain;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        BayersMain bm = new BayersMain("./src/resources/wikipedia_70.arff");
+        bm.train();
+        bm.test();
     }
 }
--- src/bayers/BayersMain.java
+++ src/bayers/BayersMain.java
@@ -12,21 +12,50 @@ public class BayersMain {
     private Instances data;
     private Classifier cl;
 
-    public void init() {
-        // TODO: Not sure if this is needed....
+    public BayersMain(String fileName) {
+        this.fileName = fileName;
+        readData();
     }
 
     private void readData() {
+        System.out.println(" > Starting to read data...");
+        try {
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource(fileName);
+            Instances raw = source.getDataSet();
 
+
+            StringToWordVector stw = new StringToWordVector(1000);
+            stw.setLowerCaseTokens(true);
+            stw.setInputFormat(raw);
+
+            data = Filter.useFilter(raw, stw);
+            data.setClassIndex(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(" > Reading complete..");
     }
 
     public void train() {
+        System.out.println(" > Training started!");
+        try {
+            cl = new NaiveBayesMultinomial();
+            cl.buildClassifier(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+
+        System.out.println("Training completed.");
     }
 
     public void test() {
         try {
-
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(cl, data, 10, new java.util.Random(1));
+            System.out.println(eval.toSummaryString());
+            System.out.println(eval.toMatrixString());
         } catch (Exception e){
             e.printStackTrace();
         }
